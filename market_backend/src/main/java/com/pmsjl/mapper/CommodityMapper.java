0bf7c309a2840594a52e4448519b6a09f841ad29
pmsjl <2557661340@qq.com>
2026-04-09 08:50:15 +0800

    开始commodity接口

---
+++ market_backend/src/main/java/com/pmsjl/mapper/CommodityMapper.java
@@ -0,0 +1,19 @@
+package com.pmsjl.mapper;
+
+import com.pmsjl.model.entity.Commodity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pmsjl.model.entity.Commodity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author pmsjl
+ * @since 2026-04-07
+ */
+@Mapper
+public interface CommodityMapper extends BaseMapper<Commodity> {
+
+}
---
+++ market_backend/src/main/java/com/pmsjl/mapper/CommodityTypeMapper.java
@@ -0,0 +1,18 @@
+package com.pmsjl.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.pmsjl.model.entity.CommodityType;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author pmsjl
+ * @since 2026-04-07
+ */
+@Mapper
+public interface CommodityTypeMapper extends BaseMapper<CommodityType> {
+
+}
