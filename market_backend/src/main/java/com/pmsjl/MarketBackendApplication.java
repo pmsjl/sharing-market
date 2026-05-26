fa9f961d0243e8d804bccecfd81c12ba1204e82c
pmsjl <184948196+pmsjl@users.noreply.github.com>
2026-05-26 12:52:12 +0800

    实现浏览量的缓存读写并添加定时刷新

--- market_backend/src/main/java/com/pmsjl/MarketBackendApplication.java
+++ market_backend/src/main/java/com/pmsjl/MarketBackendApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication(exclude = { SessionAutoConfiguration.class })
 public class MarketBackendApplication {
 
